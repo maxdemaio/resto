84587d08b1aba86f90456612da93de7a9764a3fd
maxdemaio <maxdman321@gmail.com>
2023-10-02 21:38:53 -0400

    start menus

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      mantra: ["Mantra", "system-ui"],
+    },
     screens: {
       md: "950px",
     },
