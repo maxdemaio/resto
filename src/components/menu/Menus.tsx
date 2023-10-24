9351512b86e32807a27a5bba3370d839ec900474
maxdemaio <maxdman321@gmail.com>
2023-10-24 08:38:09 -0400

    active color for menu

--- src/components/menu/Menus.tsx
+++ src/components/menu/Menus.tsx
@@ -9,15 +9,17 @@ const Menus: React.FC = () => {
     setSelectedCategory(category);
   };
 
+  const isButtonActive = (category: string) => {
+    return selectedCategory === category ? 'active' : '';
+  };
+
   return (
       <section id="menus" className="bg-[#895621] text-white px-3 md:px-[10rem] py-14">
         <h2 className="font-mantra text-5xl md:text-6xl mb-6">Menus</h2>
-        <div
-          className="flex items-stretch justify-between w-full border-b mb-16"
-        >
-          <button onClick={() => handleCategoryClick('Food')} className="grow bg-black p-4 font-mantra text-xl md:text-3xl basis-0">FOOD</button>
-          <button onClick={() => handleCategoryClick('Drinks')} className="grow bg-black p-4 font-mantra text-xl md:text-3xl basis-0">DAY DRINKING</button>
-          <button onClick={() => handleCategoryClick('More')} className="grow bg-black p-4 font-mantra text-xl md:text-3xl basis-0">COFFEE & MORE</button>
+        <div className="flex items-stretch justify-between w-full border-b mb-16">
+          <button onClick={() => handleCategoryClick('Food')} className={`grow p-4 font-mantra text-xl md:text-3xl basis-0 ${isButtonActive('Food')}`}>FOOD</button>
+          <button onClick={() => handleCategoryClick('Drinks')} className={`grow p-4 font-mantra text-xl md:text-3xl basis-0 ${isButtonActive('Drinks')}`}>DAY DRINKING</button>
+          <button onClick={() => handleCategoryClick('More')} className={`grow p-4 font-mantra text-xl md:text-3xl basis-0 ${isButtonActive('More')}`}>COFFEE & MORE</button>
         </div>
         <div>
           {selectedCategory === 'Food' && <div className="fade-in-menu border-b text-lg mb-8">ALL DAY BRUNCH</div>}
