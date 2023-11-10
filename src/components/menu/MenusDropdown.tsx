d2a2eb8cd8316d556e4f3c47b8f06a0f325bc2f6
maxdemaio <maxdman321@gmail.com>
2023-11-09 19:33:56 -0500

    fuck you

--- src/components/menu/MenusDropdown.tsx
+++ src/components/menu/MenusDropdown.tsx
@@ -35,8 +35,8 @@ const Menus: React.FC = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
+            strokeWidth="2"
+            strokeLinecap="round"
             strokeLinejoin="round"
             className={
               isDropdownOpen
