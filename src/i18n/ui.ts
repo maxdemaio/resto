53c1759477bc0f1580910d6adbf96b6527e209c6
maxdemaio <maxdman321@gmail.com>
2023-11-12 15:53:32 -0500

    fin hero

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,15 +5,31 @@ export const languages = {
 
 export const defaultLang = 'en';
 
+// {t("")}
 export const ui = {
   en: {
     'nav.about': 'ABOUT',
     'nav.lang': 'FRANÇAIS',
-    'nav.langmob': 'FR'
+    'nav.langmob': 'FR',
+    'hero.coffee': 'Coffee',
+    'hero.wine': 'Natural wines',
+    'hero.brunchdescrip': 'from 10am to 4pm',
+    'hero.coffeedescrip': 'for here or to go',
+    'hero.lunchdescrip': 'business or boozy',
+    'hero.winedescrip': 'many by the glass',
+    'hero.snacksdescrip': 'all day long',
   },
   fr: {
     'nav.about': 'À PROPOS',
     'nav.lang': 'ENGLISH',
-    'nav.langmob': 'EN'
+    'nav.langmob': 'EN',
+    'hero.coffee': 'Café',
+    'hero.wine': 'Vins nature',
+    'hero.brunchdescrip': 'de 10h à 16h',
+    'hero.coffeedescrip': 'pour ici ou pour emporter',
+    'hero.lunchdescrip': 'd’affaires ou arrosé',
+    'hero.winedescrip': 'plusieurs offerts au verre',
+    'hero.snacksdescrip': 'pour toute la journée',
+
   },
 } as const;
