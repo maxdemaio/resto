1ff6c793d16309d741f3fe490686bf76d4b8de37
maxdemaio <maxdman321@gmail.com>
2023-11-12 16:09:33 -0500

    fin about

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -18,6 +18,10 @@ export const ui = {
     'hero.lunchdescrip': 'business or boozy',
     'hero.winedescrip': 'many by the glass',
     'hero.snacksdescrip': 'all day long',
+    'about.who': 'Who is Resto',
+    'about.owner': 'About the owner',
+    'about.whodescrip': 'Located in the heart of Old Montreal, Resto is the perfect place to enjoy a delicious breakfast or lunch, to take away coffee & pastries or delight in a long boozy brunch. The cellar offers an ambitious natural wine selection while complimenting a refined yet comforting menu.',
+    'about.ownerdescrip': 'Resto Owner, a dedicated food enthusiast, followed a traditional path to the professional kitchen. They studied culinary arts and gained experience at renowned restaurants such as Le Petit Gourmet and Chez L\'Artiste. In 2017, they fulfilled their dream of opening Resto, a cozy restaurant in the heart of Old Montreal known for its exquisite dishes and warm ambiance.',
   },
   fr: {
     'nav.about': 'À PROPOS',
@@ -30,6 +34,9 @@ export const ui = {
     'hero.lunchdescrip': 'd’affaires ou arrosé',
     'hero.winedescrip': 'plusieurs offerts au verre',
     'hero.snacksdescrip': 'pour toute la journée',
-
+    'about.who': 'Qui est Resto?',
+    'about.owner': 'À propos du propriétaire',
+    'about.whodescrip': 'Situé au cœur du Vieux-Montréal, Resto est l\'endroit idéal pour savourer un délicieux petit-déjeuner ou déjeuner, emporter café et pâtisseries ou savourer un long brunch arrosé. La cave propose une sélection de vins naturels ambitieuse tout en complétant une carte raffinée mais réconfortante.',
+    'about.ownerdescrip': 'Le propriétaire de Resto, passionné de gastronomie, a suivi un chemin traditionnel jusqu\'à la cuisine professionnelle. Il a étudié les arts culinaires et acquis de l\'expérience dans des restaurants renommés tels que Le Petit Gourmet et Chez L\'Artiste. En 2017, il a réalisé son rêve d\'ouvrir Resto, un restaurant chaleureux au cœur du Vieux-Montréal reconnu pour ses plats exquis et son ambiance chaleureuse.',
   },
 } as const;
